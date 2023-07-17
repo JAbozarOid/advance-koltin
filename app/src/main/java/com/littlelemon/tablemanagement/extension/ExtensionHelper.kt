b2083d0902aaa8d35454920406f56a62c25d722f
JAbozarOid <araghibdoust@hotmail.com>
2023-07-17 07:23:41 +0330

    dish_entity

--- app/src/main/java/com/littlelemon/tablemanagement/extension/ExtensionHelper.kt
+++ app/src/main/java/com/littlelemon/tablemanagement/extension/ExtensionHelper.kt
@@ -1,9 +1,9 @@
 package com.littlelemon.tablemanagement.extension
 
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.appcompat.widget.AppCompatTextView
+import com.littlelemon.tablemanagement.entity.Dish
 
 fun ComponentActivity.showMessage(text: String) {
     Toast.makeText(this, text, Toast.LENGTH_LONG).show()
@@ -16,7 +16,15 @@ fun Int.getFormattedAmount(): String {
         "Out of Stock"
 }
 
-fun AppCompatTextView.appendText(textToAppend : String) {
+fun AppCompatTextView.appendText(textToAppend: String) {
     val newText = "$text \n $textToAppend"
     text = newText
+}
+
+fun Dish.printIngredients() {
+    println(this.ingredients)
+}
+
+fun Dish.removeSalt(ingredient: String) {
+    this.ingredients.remove(ingredient)
 }
